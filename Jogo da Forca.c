21f922fd409b79b5d13cf84c7505e9f7c9dd4cda
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2023-11-30 22:03:46 -0300

    Update
    Hoje coloquei todo o código de uma forma que fique fácil de ser lido, ele esta todo declarado em "funções".

--- Jogo da Forca.c
+++ Jogo da Forca.c
@@ -3,6 +3,7 @@
 
 void abertura() {printf("   _     _     _       _     _     _     _     _    \n");printf("  / \\   / \\   / \\     / \\   / \\   / \\   / \\   / \\   \n");printf(" ( B ) ( E ) ( M )   ( V ) ( I ) ( N ) ( D ) ( O )  \n");printf("  \\_/   \\_/   \\_/     \\_/   \\_/   \\_/   \\_/   \\_/   \n");printf("J O G O - D A - F O R C A\n\n");
 }
+
 void chuta(char chutes[], int* tentativas) {
 	char chute;
 	printf("Qual e o seu chute ??\n\n");
@@ -11,6 +12,19 @@ void chuta(char chutes[], int* tentativas) {
 	chutes[*tentativas] = chute;
 	(*tentativas)++;
 }
+
+void desenhaforca(char* palavrasecreta, char* chutes, int tentativas) {
+	printf("Voce ja deu %d chutes\n", tentativas);
+
+	for(int i = 0; i < strlen(palavrasecreta); i++) {
+		if(jachutou(palavrasecreta[0], chutes, tentativas)) {
+		prinft("%c ", palavrasecreta[i]);
+		} else {
+			printf("_ ");
+		}
+	}
+}
+
 int jachutou(char letra, char* chutes, int tentativas) {
 	int achou = 0;
 
@@ -20,30 +34,14 @@ int jachutou(char letra, char* chutes, int tentativas) {
 			break;
 		}
 	}
-	
+
 	return achou;
 }
-
-
-
-
 int main() {
 
-	int numeros[3];
-
-	int* ponteiros = numeros;
-	numeros[0] = 10;
-	numeros[1] = 20;
-	numeros[2] = 30;
-
-	for(int i = 0; 1 < 3; i++){
-	printf("%d ", numeros[0]);
-
-	printf("%d ", *(ponteiros + 1));
-	}
     abertura(); 
 
-    printf("Escolha uma Letra:\n\n");
+    printf("Escolha uma letra:\n\n");
 
 	char palavrasecreta[20];
 	sprintf(palavrasecreta, "MELANCIA");
@@ -55,21 +53,9 @@ int main() {
 	int tentativas = 0;
 
 	do {
-		
-		for(int i = 0; i < strlen(palavrasecreta); i++) {
-			int achou = 0; 
 
-			jachutou(palavrasecreta[i], chutes, tentativas);
-
-			if(achou) {
-				printf("%c ", palavrasecreta[i]);
-			} else {
-				printf("_ ");
-			}
-		}
-		printf("\n");
-
+		desenhaforca(palavrasecreta, chutes, tentativas);
 		chuta(chutes, &tentativas);
-		printf("Voce ja chuteu %d vezes vai certar ou nao ??\n\n", tentativas);
+
 	} while (!acertou && !enforcou);
 }
